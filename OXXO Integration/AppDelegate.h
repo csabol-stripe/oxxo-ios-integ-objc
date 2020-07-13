053eb742d2d8e4a9cee73ecb5dacc9a7c247f0fb
Cameron Sabol <csabol@stripe.com>
2020-07-13 11:11:19 -0700

    Initial Commit

---
+++ OXXO Integration/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OXXO Integration
+//
+//  Created by Cameron Sabol on 7/13/20.
+//  Copyright © 2020 Stripe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OXXO Integration/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  OXXO Integration
+//
+//  Created by Cameron Sabol on 7/13/20.
+//  Copyright © 2020 Stripe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
